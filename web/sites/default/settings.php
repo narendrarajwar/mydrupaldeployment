75ed0bbc3c494dab148459d4def634ff9fecf181
Narendra Rajwar <narendra.rajwar27@gmail.com>
2024-02-12 15:19:23 +0530

    pantheon setup.

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -32,3 +32,4 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+$settings['config_sync_directory'] = '../config/sites/default/sync';
